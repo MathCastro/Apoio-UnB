96d463e65120b0e5cd75d9276fff12c4c64e82a9
Rodrigo <rodrigo.demetrio.palma@gmail.com>
2019-06-03 16:01:53 -0300

    adding the remaining colors

--- lib/res/Colors.dart
+++ lib/res/Colors.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 final mutedBlue = Color(0xff33849b);
 final aquamarine = Color(0xff03dac6);
+final lightBlueGrey = Color(0xffd7eeeb);
+final lightBlueGreyTwo = Color(0xffdaf5ee);
