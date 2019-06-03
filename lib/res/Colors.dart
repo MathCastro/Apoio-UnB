56fc90bc5f76358c9016476b4ed28d15ace54ce8
Rodrigo <rodrigo.demetrio.palma@gmail.com>
2019-06-03 16:25:15 -0300

    padronizando nome de cores

--- lib/res/Colors.dart
+++ lib/res/Colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-final mutedBlue = Color(0xff33849b);
-final aquamarine = Color(0xff03dac6);
-final lightBlueGrey = Color(0xffd7eeeb);
-final lightBlueGreyTwo = Color(0xffdaf5ee);
+final colorMutedBlue = Color(0xff33849b);
+final colorAquamarine = Color(0xff03dac6);
+final colorLightBlueGrey = Color(0xffd7eeeb);
+final colorLightBlueGreyTwo = Color(0xffdaf5ee);
