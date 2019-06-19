b44b5a1cb3b5d6d7e618d93a0cf6a6b1c05b848b
MathCastro <matheusfk74@gmail.com>
2019-06-19 18:23:39 -0300

    pega aí rodrigão

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'view/LoginView.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,7 +21,10 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginView(),
+      }
     );
   }
 }
---
+++ lib/view/LoginView.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Card(
+        child: Text("dasdasdsa"),
+      )
+    );
+  }
+}
