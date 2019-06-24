22f06883a89052a4ef657584540e037e74c30eec
MathCastro <matheusfk74@gmail.com>
2019-06-24 18:08:00 -0300

    loginview

--- lib/view/LoginView.dart
+++ lib/view/LoginView.dart
@@ -3,19 +3,24 @@ import '../res/Colors.dart';
 
 import 'package:flutter/material.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        height: height,
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: <Widget>[
-            Positioned(
-              top: height * 0.75 - 125,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 370.0),
+          child: Center(
+            child: Container(
               child: Card(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
@@ -23,7 +28,7 @@ class LoginView extends StatelessWidget {
                 elevation: 4,
                 child: Container(
                   width: width - 72,
-                  height: 250,
+                  height: 253,
                   child: Column(
                     children: <Widget>[
                       Padding(
@@ -38,28 +43,107 @@ class LoginView extends StatelessWidget {
                               fontSize: 20.0),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8.0, left: 20),
-                        child: Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            "Matrícula:",
-                            style: TextStyle(
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8.0, left: 20),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "Matrícula:",
+                                  style: TextStyle(
+                                      color: colorMutedBlue,
+                                      fontWeight: FontWeight.w700,
+                                      fontFamily: "Roboto",
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 14.0),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 20, right: 20, top: 4),
+                              child: Container(
+                                height: 35,
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color:
+                                            colorMutedBlue, //Color of the border
+                                        style: BorderStyle
+                                            .solid, //Style of the border
+                                        width: 0.8, //width of the border
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8.0, left: 20),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "Senha:",
+                                  style: TextStyle(
+                                      color: colorMutedBlue,
+                                      fontWeight: FontWeight.w700,
+                                      fontFamily: "Roboto",
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 14.0),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 20, right: 20, top: 4),
+                              child: Container(
+                                height: 35,
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color:
+                                            colorMutedBlue, //Color of the border
+                                        style: BorderStyle
+                                            .solid, //Style of the border
+                                        width: 0.8, //width of the border
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: RaisedButton(
                                 color: colorMutedBlue,
-                                fontWeight: FontWeight.w700,
-                                fontFamily: "Roboto",
-                                fontStyle: FontStyle.normal,
-                                fontSize: 14.0),
-                          ),
+                                onPressed: () {
+                                  Navigator.pushNamed(context, '/login');
+                                },
+                                child: Center(
+                                  child: Text(
+                                    "Login",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      
                     ],
                   ),
                 ),
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
